95c38ec30d562c17e4b59ec5f88b3088108fcb14
Greg Cooke <gtcooke94@gmail.com>
2019-10-03 15:22:17 -0400

    Completed ABC bite

--- bites/24.ABCs/challenge.py
+++ bites/24.ABCs/challenge.py
@@ -2,12 +2,41 @@ from abc import ABC, abstractmethod
 
 
 class Challenge(ABC):
-    pass
+    def __init__(self, number, title):
+        self.number = number
+        self.title = title
+
+    @abstractmethod
+    def verify(self, val):
+        pass
+
+    @property
+    @abstractmethod
+    def pretty_title(self):
+        pass
 
 
 class BlogChallenge(Challenge):
-    pass
+    def __init__(self, number, title, merged_prs):
+        super().__init__(number, title)
+        self.merged_prs = merged_prs
+
+    def verify(self, val):
+        return val in self.merged_prs
+
+    @property
+    def pretty_title(self):
+        return f"PCC1 - {self.title}"
 
 
 class BiteChallenge(Challenge):
-    pass
+    def __init__(self, number, title, result):
+        super().__init__(number, title)
+        self.result = result
+
+    def verify(self, val):
+        return val == self.result
+
+    @property
+    def pretty_title(self):
+        return f"Bite {self.number}. {self.title}"
