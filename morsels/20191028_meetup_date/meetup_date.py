68ab39067cf9f696bfac48bf7acc4d4d82b7f2ad
Greg Cooke <gtcooke94@gmail.com>
2019-10-29 14:43:57 -0400

    Beginning work and first bonus

--- morsels/20191028_meetup_date/meetup_date.py
+++ morsels/20191028_meetup_date/meetup_date.py
@@ -1,2 +1,15 @@
-def meetup_date():
-    pass
+import datetime
+def meetup_date(year, month, nth=4, weekday=3):
+    current = datetime.date(year, month, 1)
+    current_nth = 1
+    while current.weekday() != weekday:
+        current = advance_days(current, 1)
+    while current_nth != nth:
+        current = advance_days(current, 7)
+        current_nth += 1
+    return current
+
+def advance_days(current, days):
+    return current + datetime.timedelta(days=days)
+
+
