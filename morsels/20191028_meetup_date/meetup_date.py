d16008736278d95d2457d1715a1b010eabc85c38
Greg Cooke <gtcooke94@gmail.com>
2019-10-29 14:48:12 -0400

    Can specify from end of the month

--- morsels/20191028_meetup_date/meetup_date.py
+++ morsels/20191028_meetup_date/meetup_date.py
@@ -1,11 +1,26 @@
 import datetime
+import calendar
+
 def meetup_date(year, month, nth=4, weekday=3):
-    current = datetime.date(year, month, 1)
-    current_nth = 1
+    if nth > 0:
+        # Start from the beginning of the month
+        current = datetime.date(year, month, 1)
+        current_nth = 1
+        advance_day = 1
+        advance_week = 7
+    else:
+        # Start from the end of the month ex: nth = -2, weekday = 3 would be the second to last Thursday of the month
+        current = datetime.date(year, month, calendar.monthrange(year, month)[1])
+        current_nth = 1
+        advance_day = -1
+        advance_week = -7
+        nth = -nth
+
+
     while current.weekday() != weekday:
-        current = advance_days(current, 1)
+        current = advance_days(current, advance_day)
     while current_nth != nth:
-        current = advance_days(current, 7)
+        current = advance_days(current, advance_week)
         current_nth += 1
     return current
 
